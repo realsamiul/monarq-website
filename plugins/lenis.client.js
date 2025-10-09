db4066d12ff17f1eb9dd23380b54c0a1b73804ff
realsamiul <realsamiulkarim@gmail.com>
2025-10-09 08:50:05 +0600

    Fix: Re-enable Lenis and remove CSS scroll override

--- plugins/lenis.client.js
+++ plugins/lenis.client.js
@@ -2,28 +2,28 @@ import Lenis from 'lenis'
 
 export default defineNuxtPlugin(() => {
   if (process.client) {
-    // const lenis = new Lenis({
-    //   duration: 1.2,
-    //   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    //   orientation: 'vertical',
-    //   gestureOrientation: 'vertical',
-    //   smoothWheel: true,
-    //   wheelMultiplier: 1,
-    //   smoothTouch: false,
-    //   touchMultiplier: 2,
-    //   infinite: false,
-    // })
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    })
 
-    // function raf(time) {
-    //   lenis.raf(time)
-    //   requestAnimationFrame(raf)
-    // }
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
 
-    // requestAnimationFrame(raf)
+    requestAnimationFrame(raf)
 
     return {
       provide: {
-        // lenis
+        lenis
       }
     }
   }
